336cf6c4c2da761c4522352fc41f296020632ce0
azizbns11 <azizbensalem444@gmail.com>
2025-03-08 17:48:15 +0000

    Add new /status endpoint

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello, CI/CD!") 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, CI/CD!"
+
+@app.route('/status')
+def status():
+    return jsonify({"status": "running", "message": "API is up and running!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,6 +1,21 @@
-import unittest 
-class TestApp(unittest.TestCase): 
-    def test_output(self): 
-        self.assertTrue(True) 
-if __name__ == "__main__": 
-    unittest.main() 
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b"Hello, CI/CD!")
+
+    def test_status_route(self):
+        response = self.app.get('/status')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"status": "running", "message": "API is up and running!"})
+
+if __name__ == '__main__':
+    unittest.main()
